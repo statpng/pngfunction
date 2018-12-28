521702ba6c5bab65dc53101906edde845ac26626
statpng <kkp7700@naver.com>
2018-12-28 20:53:32 +0900

    Integrated version.. In later, I will decide whethere the functions are stored seperately.

--- pngfunction.R
+++ pngfunction.R
@@ -84,6 +84,50 @@ for( j in 1:length(seq.alpha) ){
 ####################################################
 
 ####################################################
+mglmnet.selprob <- function(SNP, Phenotype, idx=NULL, Alpha=NULL, nlambda=10, psub=0.5, K=100, cov=c("none","remainder","pc")){
+  if( NCOL(Phenotype)>1 & is.null(idx) ) stop("Include an integer of idx")
+  if( NCOL(Phenotype)==1 & !is.null(idx) ) stop("Include a data.frame or matrix of Phenotype")
+  
+  M <- ncol(Phenotype)
+  
+  n <- NROW(Phenotype);
+  nsub <- n*psub;
+  if(is.null(Alpha)) Alpha <- 1:9*0.1
+  
+
+  vector.lambda <- NULL
+  for( i in 1:10 ){
+    for( j in 1:length(Alpha) ){
+      wsub <- sample(n, nsub)
+      SNPsub <- SNP[wsub,]
+      Phenotypesub <- Phenotype[wsub, ]
+      fitsub <- mglmnet(x=SNPsub, y=Phenotypesub, idx=idx, alpha=Alpha[j], family="gaussian", cov=cov)
+      vector.lambda <- c( vector.lambda, fitsub$lambda )
+    }
+  }
+  
+  lambda.min <- min(vector.lambda)
+  lambda.max <- max(vector.lambda)
+  
+  seq.lambda <- seq(lambda.min, lambda.max, length.out=nlambda);
+  out <- array(0, c(ncol(SNP), length(seq.lambda), length(Alpha)) );
+  for( i in 1:K ){
+    for( j in 1:length(Alpha) ){
+      wsub <- sample(n, nsub);
+      SNPsub <- SNP[wsub,];
+      Phenotypesub <- Phenotype[wsub, ];
+      mglmnet.fit <- mglmnet(x=SNPsub, y=Phenotypesub, idx=idx, family="gaussian", alpha=Alpha[j], lambda=seq.lambda, cov=cov)
+      out[,,j] <- out[,,j] + as.numeric( mglmnet.fit$beta!=0 ) ;
+    }
+  }
+  
+  return(out);
+  
+}
+####################################################
+
+
+####################################################
 tpr.top <- function(sp, top, true){
     sp.sort <- sort( sp, decreasing=TRUE )
     thr <- sp.sort[top]
@@ -255,3 +299,189 @@ gaussian.glmnet.sp <- function(x, y, alpha, lambda=NULL, K=100, psub=0.5, nlamb=
   )
 }
 ####################################################
+
+
+
+####################################################
+GenSimulation.png <- function(n, p, M=25, snp.rho=0.6, ptrue=c(0.001, 0.005, 0.01), Beta=1, Beta.pdec=0.1, pcross=0.2,
+                              y.pNeg=0.2, Omega=0, rho=0.3){
+  
+  # snp <- do.call( "cbind", lapply(MAF, function(x) rbinom(n, 2, x) ) )
+  GenSNP <- png.GenSNP(n=n, p=p, rho=snp.rho)
+  snp <- GenSNP$snp
+  MAF <- GenSNP$MAF
+  dimnames(snp) <- list(paste0("N",1:n), paste0("snp", 1:p))
+  ##  range( cor(snp)[upper.tri(matrix( 0, ncol(snp), ncol(snp) ) )] )
+  
+  K <- p*ptrue
+  ponly <- 1-pcross
+  Swhole <- 1:floor(pcross*K)
+  Sonly <- outer( 100*seq_len(M), (seq_len( ceiling(ponly*K)) ), "+" )
+  wbeta <- as.matrix( cbind(matrix(replicate(nrow(Sonly), Swhole), nrow=nrow(Sonly), byrow=TRUE), Sonly)) 
+  NameWhole <- paste0(Swhole, "(Whole)")
+  if( pcross==1 ){
+    NameOnly <- NA
+  } else{ 
+    NameOnly <- paste0(length(Swhole)+seq_len(ncol(Sonly)), "(Only)")
+  }
+  Colname_wbeta <- as.character( na.omit( c( NameWhole, NameOnly ) ) )
+  dimnames(wbeta) <- list(paste0("Phenotype.",1:M), Colname_wbeta )
+  beta <- matrix(0, nrow=p, ncol=M)
+  for( m in 1:M ){
+    beta[Swhole, m]    <- Beta*(--1)^(m+1)
+    if( pcross != 1 ){
+      beta[Sonly[m,], m] <- (--1)^(m)*(Beta-Beta.pdec*(sapply(Sonly[m,], digits, which=1)-1))
+    }
+  }
+  
+  ##  beta[1:10,1:m]
+  ##  beta[101:110, 1:2]
+  ##  beta[201:210, 1:2]
+  ##  hist( MAF[as.numeric(wbeta)] )
+  
+  e.varcov <- GenVarcov(M, PropOfNeg = y.pNeg, Omega=Omega, rho=rho)
+  e.varcov[upper.tri(e.varcov)] %>% sign %>% table
+  y <- snp %*% beta + mnormt::rmnorm(n,varcov=e.varcov)
+#  corrplot::corrplot(cor(y), tl.pos = "n")
+  ValuesOfArguments = list(n=n, p=p, M=M, snp.rho=snp.rho, ptrue=ptrue, Beta=Beta, pcross=pcross,
+                           y.pNeg=y.pNeg, Omega=Omega, rho.y = rho)
+  return(list(snp=snp, y=y, MAF=MAF, beta=beta, wbeta=wbeta, e.varcov=e.varcov, args = ValuesOfArguments));
+}
+####################################################
+
+####################################################
+mglmnet <- function(x=x, y=y, idx=NULL, cov=c("none", "remainder", "pc"), ...){
+  yidx <- y[,idx]
+  if(cov == "none"){
+    return( glmnet(x=x, y=yidx, ...) )
+  } else{
+    if( NCOL(y)<2 ) stop("y should be a data.frame or matrix")
+    if(is.null(idx)) stop("idx must be needed");
+    yidx <- y[,idx]
+    Remainder <- y[,-idx]
+    if(cov == "remainder") {
+      PF <- c( rep( 0, NCOL(Remainder) ), rep( 1, ncol(x) ) )
+      out <- glmnet(x=cbind(Remainder, x), y=yidx, penalty.factor=PF, ...)
+      out$dim[1] <- ncol(x)
+      out$beta <- out$beta[-c(1:NCOL(Remainder)),]
+      
+      
+      return( out )
+    } else if(cov == "pc"){
+      PC <- prcomp(Remainder, scale.=TRUE)
+      PCcov <- PC$x[,which( summary( PC )$sdev > 1), drop=FALSE]
+      PF <- c( rep( 0, NCOL(PCcov) ), rep( 1, ncol(x) ) )
+      out <- glmnet(x=cbind(PCcov, x), y=yidx, penalty.factor=PF, ...)
+      out$dim[1] <- ncol(x)
+      out$beta <- out$beta[-c(1:NCOL(PCcov)),]
+      return( out )
+    }
+  }
+}
+####################################################
+
+####################################################
+Method.lm <- function(SNP, Phenotype){
+pvalue.lm <- sapply( 1:ncol(SNP), function(j) {
+  fit_coef <- summary( lm( Phenotype ~ SNP[,j] ) )$coef
+  ifelse( nrow(fit_coef)==2, fit_coef[2,4], 9999 )
+} )
+return(-log10(pvalue.lm))
+}
+####################################################
+
+####################################################
+mcc <- function(true, pred, p){
+  data.frame(
+    Actual = ifelse((1:p)%in% true, "TRUE", "FALSE"),
+    Condition = ifelse((1:p)%in% pred, "+", "-") ) %>% table -> TABLE
+  TP <- TABLE[2,2]
+  FP <- TABLE[1,2]
+  FN <- TABLE[2,1]
+  TN <- TABLE[1,1]
+  return( (TP*TN-FP*FN)/(sqrt(TP+FP)*sqrt(TP+FN)*sqrt(TN+FP)*sqrt(TN+FN)) );
+}
+####################################################
+
+####################################################
+glmnet_ycov <- function( SNP, Phenotype, idxY, Alpha, method ){
+  
+  Y <- Phenotype[,idxY]
+  subY <- Phenotype[,-idxY]
+  
+  if( method == "glmnet" ){
+    cvglmnet.fit <- cv.glmnet(SNP, Y, family="gaussian", alpha=Alpha, type.measure="mse" )
+    glmnet.fit <- glmnet(SNP, Y, family="gaussian", alpha=Alpha, lambda=cvglmnet.fit$lambda.1se )
+    return( which( glmnet.fit$beta != 0 ) );
+    
+  } else if( method == "ycov" ){
+    X <- cbind(subY, SNP)
+    cvglmnet.fit <- cv.glmnet(X, Y, family="gaussian", alpha=Alpha, type.measure="mse", penalty.factor = c( rep(0,ncol(subY)), rep(1,ncol(X)-ncol(subY)) ) ) 
+    glmnet.fit <- glmnet(X, Y, family="gaussian", alpha=Alpha, lambda=cvglmnet.fit$lambda.1se, penalty.factor = c( rep(0,ncol(subY)), rep(1,ncol(X)-ncol(subY)) ) ) 
+    return( which( glmnet.fit$beta != 0 ) );
+    
+  } else if( method == "pccov"){
+    X <- cbind(SNP, Z <- prcomp(subY, scale=TRUE)$x[,1])
+    cvglmnet.fit <- cv.glmnet(X, Y, family="gaussian", alpha=Alpha, type.measure="mse", penalty.factor=c(rep(0,1),rep(1,ncol(SNP))) ) 
+    glmnet.fit <- glmnet(X, Y, family="gaussian", alpha=Alpha, lambda=cvglmnet.fit$lambda.1se, penalty.factor=c(rep(0,1),rep(1,ncol(SNP))) )
+    return( which( glmnet.fit$beta != 0) );
+    
+  } else if( method == "lm" ){
+    pvalue.lm <- sapply( 1:ncol(SNP), function(j) summary( lm( Y ~ SNP[,j] ) )$coef[2,4] )
+    return( pvalue.lm );
+  }
+}
+####################################################
+
+####################################################
+ARCOV_R <- function(p, rho, threads=1) outer(1:p, 1:p, function(x,y) rho^abs(x-y) )
+minmax <- function(x, lag){ 
+  out <- ( x-min(x) )/(max(x)-min(x) )
+  out 
+}
+####################################################
+
+####################################################
+SampleSign <- function(Matrix, PropOfNeg=PropOfNeg){
+  Matrix[ lower.tri(Matrix) ] <- sapply( Matrix[ lower.tri(Matrix) ], function(x) (-1)^rbinom(1,1,PropOfNeg)*x )
+  Matrix[ upper.tri(Matrix) ] <- t(Matrix)[ upper.tri(Matrix) ]
+  Matrix
+}
+digits <- function(x, which=1e1){
+  as.numeric( unlist(strsplit(as.character(x),""))[nchar(x)+1-log10(10*which)] )
+}
+####################################################
+
+####################################################
+Varcov_rho <- function(p, rho){
+  out <- matrix(rho, p, p)
+  diag(out) <- 1
+  return(out)
+}
+####################################################
+
+####################################################
+GenVarcov <- function(m, Omega = 0, PropOfNeg = 0.25, rho=0){
+  if( PropOfNeg<0 | PropOfNeg>0.5 ) stop("PropOfNeg must be in [0,0.5].");
+  
+  if( rho == 0 ){
+  e.rho <- replicate( m*(m-1)/2, runif(1,0,1) )
+  e.varcov <- matrix(1, m, m)
+  e.varcov[upper.tri(e.varcov)] <- e.rho
+  e.varcov[lower.tri(e.varcov)] <- t(e.varcov)[lower.tri(e.varcov)]
+  e.varcov <- SampleSign(e.varcov, PropOfNeg = PropOfNeg)
+  isSymmetric(e.varcov)
+  
+  x <- (e.varcov %*% e.varcov) + diag(Omega, m)
+  e.varcov2 <- (x/sqrt(diag(x)%*%t( diag(x) )))
+  
+  } else if ( rho>0 ) {
+    e.varcov2 <- Varcov_rho(p=m, rho=rho)
+  }
+  
+  # e.varcov2 %>% .[upper.tri(.)] %>% hist(main="Error-term correlation", xlab=expression(rho,"e"))
+  
+  invisible(e.varcov2)
+}
+####################################################
+
