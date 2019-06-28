2579df01aa59422d4a9bf4fa9bf0738d38afc755
statpng <43024819+statpng@users.noreply.github.com>
2019-06-28 20:53:53 +0900

    Update png.cp.glmnet.R

--- png.cp.glmnet.R
+++ png.cp.glmnet.R
@@ -67,15 +67,99 @@ png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, 
                                               alpha=seq.alpha[aa], 
                                               lambda=seq.lambda[[colcol]], 
                                               family=family,
-                                              type.multinomial="grouped")$beta, 
+png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, setseed, psub=0.5, ...){
+    library(mnormt)
+    library(glmnet)
+    library(dplyr)
+    
+    x = xx
+    y = yy
+    family = "multinomial"
+    type.multinomial = "grouped"
+    seq.alpha = 1:9 * 0.1
+    seq.lambda = res_lambda[index]
+    K = 100
+    setseed = (2019 - 06 - 28)
+    psub = 0.5
+    
+    # x=Data$snp
+    # y=Data$y
+    # family="mixed"
+    # psub=0.5
+    # seq.alpha=seqalpha
+    # n.lambda=nlambda
+    # setseed=1129
+    
+    if( NROW(y) != nrow(x) ) stop("x and y should be equal length of row")
+    if(is.null(seq.alpha)) seq.alpha <- 1:9*0.1
+    if(family=="mgaussian") standardize.response <- TRUE
+    
+    x <- as.matrix(x)
+    y <- as.matrix(y)
+    
+    n <- nrow(x);
+    p <- ncol(x);
+    nsub <- n*psub;
+    
+    
+    n.alpha <- length(seq.alpha)
+    n.lambda <- unique( sapply(seq.lambda, length) )
+    
+    beta.array <- array(NA,
+                        dim = c(p, n.alpha, n.lambda, ncol(y), K), 
+                        dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
+                                         paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
+                                         paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
+                                         paste0("", seq_len(ncol(y))), # paste0("Phenotype=", seq_len(ncol(y))),
+                                         paste0("", seq_len(K)) ) ) # paste0("Rep=", seq_len(K)) ) )
+    # names(attributes(beta.array)$dimnames) <- c("Variable", "Alpha", "Lambda", "Phenotype", "Replications")
+    beta.array <- beta.array %>% slam::as.simple_sparse_array()
+    
+    
+    
+    
+    for( kk in 1:K ){
+        print(kk)
+        start <- proc.time();
+        set.seed( setseed*kk )
+        wsub <- sample(n, nsub)
+        xsub <- x[wsub,,drop=F];
+        ysub <- y[wsub,,drop=F];
+        
+        
+        for( aa in 1:length(seq.alpha) ){
+            if( family=="mgaussian" ){
+                mgaussian.fit <- append(NULL, glmnet(x=xsub, 
+                                                     y=ysub, 
+                                                     alpha=seq.alpha[aa], 
+                                                     lambda=unlist(seq.lambda[1]), 
+                                                     family=family, 
+                                                     standardize.response=TRUE, ... )$beta )
+            }
+            
+            for( colcol in 1:ncol(y) ){
+                
+                tmp.array <- array(NA, dim = c(p, n.lambda, 1))
+                if( family=="mgaussian" ){
+                    tmp.array[,,1] <- as.numeric( mgaussian.fit[[colcol]] != 0 )
+                } else if( family=="multinomial" ){
+                    tmp.array[,,1] <- 
+                        apply( sapply( glmnet(x=xsub, 
+                                              y=ysub[,colcol,drop=F], 
+                                              alpha=seq.alpha[aa], 
+                                              lambda=seq.lambda[[colcol]], 
+                                              family=family,
+                                              type.multinomial="grouped", ...)$beta, 
                                        function(xx) as.numeric(xx != 0) ), 1, sum )
                 } else {
-                    beta.array[,aa,,colcol,kk] <- as.numeric( glmnet(x=xsub, 
-                                                                     y=ysub[,colcol,drop=F], 
-                                                                     alpha=seq.alpha[aa], 
-                                                                     lambda=seq.lambda[[colcol]], 
-                                                                     family=family, ... )$beta != 0 )
+                    tmp.array[,,1] <- as.numeric( glmnet(x=xsub, 
+                                                         y=ysub[,colcol,drop=F], 
+                                                         alpha=seq.alpha[aa], 
+                                                         lambda=seq.lambda[[colcol]], 
+                                                         family=family, ... )$beta != 0 )
                 }
+                
+                beta.array[,aa,,colcol,kk] <- tmp.array[,,1]
             }
             
         }
