6e4f009ed73de1469dc687694589de30b244bb4e
statpng <43024819+statpng@users.noreply.github.com>
2019-06-28 21:11:38 +0900

    Update png.cp.glmnet.R

--- png.cp.glmnet.R
+++ png.cp.glmnet.R
@@ -37,17 +37,17 @@ png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, 
     n.lambda <- unique( sapply(seq.lambda, length) )
     
     if( out == "beta" ){
-    beta.array <- array(NA,
-                        dim = c(p, n.alpha, n.lambda, ncol(y), K), 
-                        dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
-                                         paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
-                                         paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
-                                         paste0("", seq_len(ncol(y))), # paste0("Phenotype=", seq_len(ncol(y))),
-                                         paste0("", seq_len(K)) ) ) # paste0("Rep=", seq_len(K)) ) )
-    names(attributes(beta.array)$dimnames) <- c("Variable", "Alpha", "Lambda", "Phenotype", "Replications")
+        beta.array <- array(NA,
+                            dim = c(p, n.alpha, n.lambda, ncol(y), K), 
+                            dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
+                                             paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
+                                             paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
+                                             paste0("", seq_len(ncol(y))), # paste0("Phenotype=", seq_len(ncol(y))),
+                                             paste0("", seq_len(K)) ) ) # paste0("Rep=", seq_len(K)) ) )
+        names(attributes(beta.array)$dimnames) <- c("Variable", "Alpha", "Lambda", "Phenotype", "Replications")
     } else if (out == "sp"){
         beta.array <- array(NA,
-                            dim = c(p, n.alpha, n.lambda, ncol(y)), 
+                            dim = c(p, n.alpha, n.lambda, ncol(y), 1), 
                             dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
                                              paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
                                              paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
@@ -94,10 +94,10 @@ png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, 
                                        function(xx) as.numeric(xx != 0) ), 1, sum )
                 } else {
                     tmp.selected <- as.numeric( glmnet(x=xsub, 
-                                                         y=ysub[,colcol,drop=F], 
-                                                         alpha=seq.alpha[aa], 
-                                                         lambda=seq.lambda[[colcol]], 
-                                                         family=family, ... )$beta != 0 )
+                                                       y=ysub[,colcol,drop=F], 
+                                                       alpha=seq.alpha[aa], 
+                                                       lambda=seq.lambda[[colcol]], 
+                                                       family=family, ... )$beta != 0 )
                 }
                 
                 if( out == "beta" ){
@@ -117,6 +117,7 @@ png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, 
     return(beta.array);
     
 }
+
 
 
 
