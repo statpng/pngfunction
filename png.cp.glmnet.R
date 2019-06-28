b7f99386da3004b5a46416b729299475188e62c8
statpng <43024819+statpng@users.noreply.github.com>
2019-06-28 19:56:24 +0900

    Update png.cp.glmnet.R

--- png.cp.glmnet.R
+++ png.cp.glmnet.R
@@ -1,89 +1,91 @@
-png.cp.glmnet <- function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, setseed, psub=0.5, ...){
-        library(mnormt)
-        library(glmnet)
-        library(dplyr)
+function(x, y, family, seq.alpha=NULL, seq.lambda=NULL, K=100, setseed, psub=0.5, ...){
+    library(mnormt)
+    library(glmnet)
+    library(dplyr)
+    
+    # x=Data$snp
+    # y=Data$y
+    # family="mixed"
+    # psub=0.5
+    # seq.alpha=seqalpha
+    # n.lambda=nlambda
+    # setseed=1129
+    
+    if( NROW(y) != nrow(x) ) stop("x and y should be equal length of row")
+    if(is.null(seq.alpha)) seq.alpha <- 1:9*0.1
+    if(family=="mgaussian") standardize.response <- TRUE
+    
+    x <- as.matrix(x)
+    y <- as.matrix(y)
+    
+    n <- nrow(x);
+    p <- ncol(x);
+    nsub <- n*psub;
+    
+    
+    n.alpha <- length(seq.alpha)
+    n.lambda <- unique( sapply(seq.lambda, length) )
+    
+    beta.array <- array(NA,
+                        dim = c(p, n.alpha, n.lambda, ncol(y), K), 
+                        dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
+                                         paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
+                                         paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
+                                         paste0("", seq_len(ncol(y))), # paste0("Phenotype=", seq_len(ncol(y))),
+                                         paste0("", seq_len(K)) ) ) # paste0("Rep=", seq_len(K)) ) )
+    names(attributes(beta.array)$dimnames) <- c("Variable", "Alpha", "Lambda", "Phenotype", "Replications")
+    beta.array <- beta.array %>% slam::as.simple_sparse_array()
+    
+    
+    
+    for( kk in 1:K ){
+        print(kk)
+        start <- proc.time();
+        set.seed( setseed*kk )
+        wsub <- sample(n, nsub)
+        xsub <- x[wsub,,drop=F];
+        ysub <- y[wsub,,drop=F];
         
-        # x=Data$snp
-        # y=Data$y
-        # family="mixed"
-        # psub=0.5
-        # seq.alpha=seqalpha
-        # n.lambda=nlambda
-        # setseed=1129
         
-        if( NROW(y) != nrow(x) ) stop("x and y should be equal length of row")
-        if(is.null(seq.alpha)) seq.alpha <- 1:9*0.1
-        if(family=="mgaussian") standardize.response <- TRUE
-        
-        x <- as.matrix(x)
-        y <- as.matrix(y)
-        
-        n <- nrow(x);
-        p <- ncol(x);
-        nsub <- n*psub;
-        
-        
-        n.alpha <- length(seq.alpha)
-        n.lambda <- unique( sapply(seq.lambda, length) )
-        
-        beta.array <- array(NA,
-                            dim = c(p, n.alpha, n.lambda, ncol(y), K), 
-                            dimnames = list( paste0("", 1:p),  # paste0("v", 1:p), 
-                                             paste0("",seq.alpha),   # paste0("Alpha=",seq.alpha),  
-                                             paste0("", seq_len(n.lambda)), # paste0("Lambda=", seq_len(n.lambda)),
-                                             paste0("", seq_len(ncol(y))), # paste0("Phenotype=", seq_len(ncol(y))),
-                                             paste0("", seq_len(K)) ) ) # paste0("Rep=", seq_len(K)) ) )
-        
-        names(attributes(beta.array)$dimnames) <- c("Variable", "Alpha", "Lambda", "Phenotype", "Replications")
-        
-        for( kk in 1:K ){
-                print(kk)
-                start <- proc.time();
-                set.seed( setseed*kk )
-                wsub <- sample(n, nsub)
-                xsub <- x[wsub,,drop=F];
-                ysub <- y[wsub,,drop=F];
-                
-                
-                for( aa in 1:length(seq.alpha) ){
-                        if( family=="mgaussian" ){
-                                mgaussian.fit <- append(NULL, glmnet(x=xsub, 
-                                                                     y=ysub, 
+        for( aa in 1:length(seq.alpha) ){
+            if( family=="mgaussian" ){
+                mgaussian.fit <- append(NULL, glmnet(x=xsub, 
+                                                     y=ysub, 
+                                                     alpha=seq.alpha[aa], 
+                                                     lambda=unlist(seq.lambda[1]), 
+                                                     family=family, 
+                                                     standardize.response=TRUE, ... )$beta )
+            }
+            
+            for( colcol in 1:ncol(y) ){
+                if( family=="mgaussian" ){
+                    beta.array[,aa,,colcol,kk] <- as.numeric( mgaussian.fit[[colcol]] != 0 )
+                } else if( family=="multinomial" ){
+                    beta.array[,aa,,colcol,kk] <- 
+                        apply( sapply( glmnet(x=xsub, 
+                                              y=ysub[,colcol,drop=F], 
+                                              alpha=seq.alpha[aa], 
+                                              lambda=seq.lambda[[colcol]], 
+                                              family=family,
+                                              type.multinomial="grouped")$beta, 
+                                       function(xx) as.numeric(xx != 0) ), 1, sum )
+                } else {
+                    beta.array[,aa,,colcol,kk] <- as.numeric( glmnet(x=xsub, 
+                                                                     y=ysub[,colcol,drop=F], 
                                                                      alpha=seq.alpha[aa], 
-                                                                     lambda=unlist(seq.lambda[1]), 
-                                                                     family=family, 
-                                                                     standardize.response=TRUE, ... )$beta )
-                        }
-                        
-                        for( colcol in 1:ncol(y) ){
-                                if( family=="mgaussian" ){
-                                        beta.array[,aa,,colcol,kk] <- as.numeric( mgaussian.fit[[colcol]] != 0 )
-                                } else if( family=="multinomial" ){
-                                        beta.array[,aa,,colcol,kk] <- 
-                                                apply( sapply( glmnet(x=xsub, 
-                                                                      y=ysub[,colcol,drop=F], 
-                                                                      alpha=seq.alpha[aa], 
-                                                                      lambda=seq.lambda[[colcol]], 
-                                                                      family=family,
-                                                                      type.multinomial="grouped")$beta, 
-                                                               function(xx) as.numeric(xx != 0) ), 1, sum )
-                                } else {
-                                        beta.array[,aa,,colcol,kk] <- as.numeric( glmnet(x=xsub, 
-                                                                                         y=ysub[,colcol,drop=F], 
-                                                                                         alpha=seq.alpha[aa], 
-                                                                                         lambda=seq.lambda[[colcol]], 
-                                                                                         family=family, ... )$beta != 0 )
-                                }
-                        }
-                        
+                                                                     lambda=seq.lambda[[colcol]], 
+                                                                     family=family, ... )$beta != 0 )
                 }
-                end <- proc.time();
-                cat("In this iteration, the elaspsed time=", (end-start)[3], "\n");
-                cat("The remained time is", (end-start)[3]*(K-kk), "\n");
+            }
+            
         }
-        
-        return(beta.array);
-        
+        end <- proc.time();
+        cat("In this iteration, the elaspsed time=", (end-start)[3], "\n");
+        cat("The remained time is", (end-start)[3]*(K-kk), "\n");
+    }
+    
+    return(beta.array);
+    
 }
 
 
