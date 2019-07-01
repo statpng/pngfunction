6f9b6f0c5d31f452437c9b0a989c7e49670deb35
statpng <43024819+statpng@users.noreply.github.com>
2019-07-01 15:34:15 +0900

    Update png.hapmap.R

--- png.hapmap.R
+++ png.hapmap.R
@@ -2,36 +2,37 @@ png.hapmap <- function(x){
   # Initial of function -----------------------------------------------------  
   # 1.Sorting -----------------------------------------------------------------
   ord.x <- gtools::mixedorder(colnames(x[,-(1:11)]))
-  
+
+
   x <- x[,c(1:11, ord.x+11)]
-  
+
   rm.missing <- which( x[,-(1:11)] %>% apply(1, function(x) mean(is.na(x) | x == "NN")) > 0.2 ) # 2804
   rm.missing.sample <- which( x[,-(1:11)] %>% apply(2, function(x) mean(is.na(x) | x == "NN")) > 0.2 ) # 1
-  
+
   set.heterozygote <- apply(expand.grid(c("T","C","A","G"), c("T","C","A","G")) %>% 
                               filter(Var1 != Var2), 1, paste0, collapse="")
-  
+
   png.heterozygousCalls <- function(x){
     x %>% replace( ., .=="NN", NA ) %>% unlist %>% { mean( . %in% set.heterozygote ) }
   }
-  
+
   rm.hetero <- which( x[,-(1:11)] %>% apply(1, function(x) png.heterozygousCalls(x)) > 0.2 ) # 37
-  
+
   print( x[,-(1:11)][ rm.hetero , ] %>% apply(1, table) )
-  
+
   rm.dup.var <- which(duplicated(x)) # There are no duplicates in variants
   rm.dup.sample <- which(duplicated(t(x[,-(1:11)]))) # There are no duplicates in samples
-  
+
   rm.var <- c(rm.missing, rm.hetero, rm.missing.sample, rm.dup.var)
   rm.sample <- rm.dup.sample
-  
+
   x.removed <- x
   if( length(rm.var)>0 ) x.removed <- x[ -unique( rm.var ), ]
   if( length(rm.sample)>0 ) x.removed <- x[, -(unique( rm.sample )+11) ]
-  
+
   print( dim(x) ) # 49683 x 395
   print( dim(x.removed) ) # 46852 x 395
-  
+
   myX <- as.matrix( rbind( colnames(x.removed), x.removed ) )
   myGD <- apply(myX[-1,-(1:11)], 1,
                 function(one) GAPIT.Numericalization(one, bit=2, impute="None", Major.allele.zero=TRUE)) %>% 
@@ -41,10 +42,10 @@ png.hapmap <- function(x){
 
   myX.impute <- rbind( myX[1,], cbind( myX[-1,(1:11)], t( apply(t(myX[-1,-(1:11)]), 2, png.impute.snp) ) ) )
   myGD.impute <- apply(myGD, 2, png.impute.numeric)
-                
+
   Taxa <- as.character( myX[1, -(1:11)] )
   out = list (myX=myX, myGD=data.frame(Taxa=Taxa, myGD), myGD.impute=data.frame(Taxa=Taxa, myGD.impute), myGM=myGM)#, myGT=myGT)
-  
+
   out
 }
                 
