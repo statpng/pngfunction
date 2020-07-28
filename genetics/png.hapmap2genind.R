3e2517f70c9b431e360ca005001ce1a5f3993a73
statpng <43024819+statpng@users.noreply.github.com>
2020-07-28 16:02:13 +0900

    Update png.hapmap2genind.R

--- genetics/png.hapmap2genind.R
+++ genetics/png.hapmap2genind.R
@@ -1,6 +1,7 @@
 png.hapmap2genind <- function(hapmap, ...){
   # hapmap:: (p+1) x (n+11) hapmap-formatted genotype data used in GAPIT
-  colnames(df) <- hapmap[1,-(1:11)]
+  df <- hapmap
+  colnames(df) <- as.character( unlist( hapmap[1,-(1:11)] ) )
   hapmap <- df2genind(as.data.frame(t(df)), type="codom", ncode=1, ploidy=2, ...)
   hapmap
 }
