b6b39baec1cbd91484a77e4d831c30d5b9f5913f
statpng <43024819+statpng@users.noreply.github.com>
2021-09-10 12:10:08 +0900

    Create CTGSA.R

---
+++ CTGSA/CTGSA.R
@@ -0,0 +1,67 @@
+CTGSA <- function(x, y){
+  #case,control
+  Cx1 = x[which(y==0),]
+  Tx1 = x[which(y==1),]
+  
+  ####### CTGSA #######
+  #alpha(thresholding parameter)
+  Cs1 = abs(cor(Cx1));
+  Ts1 = abs(cor(Tx1))
+  
+  Csv1 = svd(abs(Cs1))$d;
+  Tsv1 = svd(abs(Ts1))$d
+  
+  #non-coexpression (Ch,Th)
+  Ch1 = 1-Csv1[1]/sum(Csv1);
+  Th1 = 1-Tsv1[1]/sum(Tsv1)
+  
+  H_Cs1 = Cs1
+  H_Ts1 = Ts1
+  
+  Ctri1 = Cs1[upper.tri(Cs1)];
+  Ttri1 = Ts1[upper.tri(Ts1)]
+  
+  #non-coexpression(hresholding parameter)
+  Ccri1 = sort(Ctri1)[length(Ctri1)*Ch1] ;
+  Tcri1 = sort(Ttri1)[length(Ttri1)*Th1]
+  
+  #alpha(Ccri,Tcri)thresholding
+  #hard
+  H_Cs1[Cs1<Ccri1] = 0
+  H_Ts1[Ts1<Tcri1] = 0
+  
+  Csv1 = svd(abs(H_Cs1))$d;
+  Tsv1 = svd(abs(H_Ts1))$d
+  
+  #soft
+  c_soft1 = soft(H_Cs1,Ccri1);
+  t_soft1 = soft(H_Ts1,Tcri1)
+  
+  soft_Csv1 = svd(abs(c_soft1))$d;
+  soft_Tsv1 = svd(abs(t_soft1))$d
+  
+  #mcp
+  c_mcp1 = matrix(sapply( H_Cs1, function(z) mcp(z, Ccri1, gamma=2.0) ), 200, 200, byrow=FALSE )
+  t_mcp1 = matrix(sapply( H_Ts1, function(z) mcp(z, Tcri1, gamma=2.0) ), 200, 200, byrow=FALSE )
+  
+  mcp_Csv1 = svd(abs(c_mcp1))$d;
+  mcp_Tsv1 = svd(abs(t_mcp1))$d
+  
+  #scad
+  c_scad1 = matrix(sapply( H_Cs1, function(z) scad(z, Ccri1, gamma=2.0) ), 200, 200, byrow=FALSE )
+  t_scad1 = matrix(sapply( H_Ts1, function(z) scad(z, Tcri1, gamma=2.0) ), 200, 200, byrow=FALSE )
+  
+  scad_Csv1 = svd(abs(c_scad1))$d;
+  scad_Tsv1 = svd(abs(t_scad1))$d
+  
+  T_soft1=abs(soft_Tsv1[1] - soft_Csv1[1])
+  T_mcp1=abs(mcp_Tsv1[1] - mcp_Csv1[1])
+  T_scad1=abs(scad_Tsv1[1] - scad_Csv1[1])
+  T_TH1 = abs(Tsv1[1] - Csv1[1])
+  
+  list(hard=T_TH1,
+       soft=T_soft1,
+       mcp=T_mcp1,
+       scad=T_scad1)
+  
+}
