c02c966fa1b85299f476f62990be6f0f32d21e42
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2022-09-20 16:33:59 +0900

    Update UniquenessOfQuartimaxCriterion.R

--- UniquenessOfQuartimaxCriterion.R
+++ UniquenessOfQuartimaxCriterion.R
@@ -30,17 +30,19 @@ png.quartimax <- function(X, lr=0.1, eps=1e-10){
 
 out_total = NULL
 for( j in 1:100 ){
+	set.seed(j)
 	A = matrix(rnorm(10*5),10,5)
 	# A = with( svd(matrix(rnorm(20*5),10,5)), tcrossprod(u,v) );  
 
 	out = NULL; 
 	for( i in 1:100 ){ 
+		set.seed(i+100)
 		Q = with( svd(matrix(rnorm(5*5),5,5)), tcrossprod(u,v) );  
 		out[[i]] = A %*% Q
 	}
 
 	# out2 = lapply(out[1:100], function(L) png.quartimax(L)$load )
-	out2 = lapply(out[1:5], function(L) GPArotation::quartimax(L, maxit=1000, eps=1e-5)$load )
+	out2 = lapply(out[1:100], function(L) GPArotation::quartimax(L, maxit=1000, eps=1e-3)$load )
 	out3 = lapply(out2, function(x) x[,order(apply(x, 2, function(y) abs(y[1])))] )
 	out4 = sapply(out3, function(x) abs(x[1,1]) )
 	out_total[j] = mean( diff(out4) < 1e-4 ) 
