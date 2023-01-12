f77521df14b548d520434d504a016ed45932d10a
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2023-01-12 17:54:41 +0900

    Update flag_mean.R

--- flag_mean.R
+++ flag_mean.R
@@ -1,30 +1,34 @@
-flag_mean <- function(X.list, nrank=NULL, type="svd"){
+flag_mean <- function(X.list, type="svd"){
   
-  
-  if(!is.null(nrank)){
-    # X.list.orth <- lapply(X.list, function(x) qr.Q(qr(x))[,1:nrank)
-    X.list.orth <- lapply(X.list, function(X) svd(X)$u[,1:nrank])
-  } else {
-    # X.list.orth <- lapply(X.list, function(x) qr.Q(qr(x)))
-    X.list.orth <- lapply(X.list, function(X) svd(X)$u)
+  rank <- function(x){
+    sum( svd(x)$d > 1e-15 )
   }
-                        
-                        
+  
+  X.list.orth <- lapply(X.list, function(X) svd(X)$u[,1:rank(X)])
+  # X.list.orth <- lapply(X.list, function(x) qr.Q(qr(x))[,1:rank(X))
+  
   if( type == "eigen" ){
+    
     A <- Reduce("+", lapply( X.list.orth, function(x) tcrossprod( x ) ))
     # r <- sum(svd( X )$d>0)
     eigen.A <- eigen(A)
     U <- eigen.A$vectors
     L <- eigen.A$values
-  }
-  if( type == "svd" ){
+    
+  } else if( type == "svd" ){
+    
     X.orth <- do.call("cbind", X.list.orth)
-    U <- svd(X.orth)$u
+    nrank <- rank( X.orth )
+    U <- with(svd(X.orth), u[,1:nrank])
+    # if( dim(X.orth)[1] >= dim(X.orth)[2] ){
+    #   U <- with(svd(X.orth), u[,d>1e-15])
+    # } else {
+    #   U <- with(svd(t(X.orth)), v[,d>1e-15])
+    # }
+    
   }
   
-  if(!is.null(nrank)){
-    U <- U[,1:nrank]
-  }
   
   U
+  
 }
