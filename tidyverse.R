3399f1232964e8ebe62e92ca8f7e036838c1df95
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2023-03-22 14:42:27 +0900

    Update tidyverse.R

--- tidyverse.R
+++ tidyverse.R
@@ -20,3 +20,33 @@ png.label_plot <- function(label, angle) {
   }
   
   
+
+png.plot3d <- function(df, theta=135, phi=60, alpha=0.1){
+  # df: x, y, z cooridnates
+  # library(plot3D)
+  # library(ggplot2)
+  
+  trans_3d_2d <- function(data, theta=135, phi=60) {
+    pmat <- plot3D::perspbox(z=diag(2), plot=F, theta=theta, phi=phi)
+    
+    data <- as.data.frame(data)
+    XY <- plot3D::trans3D(
+      x = data$x,
+      y = data$y,
+      z = data$z,
+      pmat = pmat) %>%
+      data.frame()
+    
+    data$x <- XY$x
+    data$y <- XY$y
+    
+    return(data[, c('x', 'y')])
+  }
+
+  trans_3d_2d(df[,1:3], theta=theta, phi=phi) %>% 
+    ggplot(aes(x, y, color=x)) + geom_point(alpha=alpha)  + 
+    coord_flip() + 
+    theme(legend.position="none") + 
+    scale_y_reverse()
+  
+}
