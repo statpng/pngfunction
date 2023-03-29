1a92794707845d7b25e851116ea0b817697be462
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2023-03-29 09:47:21 +0900

    Update functions.R

--- mesh3d/functions.R
+++ mesh3d/functions.R
@@ -1,5 +1,7 @@
 # functions
 
+# devtools::install_github("zarquon42b/Morpho")
+
 png.data.nose <- function(){
   library(Morpho)
   data(nose)
@@ -32,7 +34,48 @@ png.mesh3d.plot <- function(mesh, landmark = NULL, radius = 0.1) {
   
 }
 
+png.mesh3d.rotate <- function(mesh, angle = 0) {
+  Q <- png.create_rotmat3d(angle)
+  mesh$vb[1:3,] <- t( t(mesh$vb[1:3,]) %*% Q )
+  return(mesh)
+}
 
+
+
+
+png.mesh3d.scatter2d <- function(mesh, theta=135, phi=60, alpha=0.1){
+  library(plot3D)
+  library(ggplot2)
+  
+  if (inherits(mesh, "mesh3d")) {
+    df <- as.matrix(t(mesh$vb[1:3,]))
+  }
+  colnames(df) <- c("x", "y", "z")
+  
+  trans_3d_2d <- function(data, theta=135, phi=60) {
+    pmat <- plot3D::perspbox(z=diag(2), plot=F, theta=theta, phi=phi)
+    
+    data <- as.data.frame(data)
+    XY <- plot3D::trans3D(
+      x = data$x,
+      y = data$y,
+      z = data$z,
+      pmat = pmat) %>%
+      data.frame()
+    
+    data$x <- XY$x
+    data$y <- XY$y
+    
+    return(data[, c('x', 'y')])
+  }
+
+  trans_3d_2d(df[,1:3], theta=theta, phi=phi) %>% 
+    ggplot(aes(x, y, color=x)) + geom_point(alpha=alpha)  + 
+    coord_flip() + 
+    theme(legend.position="none") + 
+    scale_y_reverse()
+  
+}
 
 
 
@@ -110,12 +153,6 @@ png.create_rotmat3d <- function(angle = 0) {
   Q <- matrix(c(cos(rotation_angle_radians), -sin(rotation_angle_radians), 0,
                 sin(rotation_angle_radians), cos(rotation_angle_radians), 0, 0, 0, 1), nrow = 3, ncol = 3, byrow = TRUE)
   return(Q)
-}
-
-png.mesh3d.rotate <- function(mesh, angle = 0) {
-  Q <- png.create_rotmat3d(angle)
-  mesh$vb[1:3,] <- t( t(mesh$vb[1:3,]) %*% Q )
-  return(mesh)
 }
 
 png.plotly.scatter3d <- function(mesh, size = 2) {
