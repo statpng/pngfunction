ddc2e9fe83ea5210738027a3f19eb14a4f2f9597
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2023-03-30 13:34:09 +0900

    Delete QnA directory

--- QnA/EmbeddingFontsInPDF.md
+++
@@ -1,8 +0,0 @@
-https://r-graphics.org/recipe-output-fonts-pdf
-
-library(extrafont)
-
-p <- ggplot2::ggplot(iris, aes(Sepal.Length, Sepal.Width, color=Species)) + geom_point()
-ggsave(p, filename = "tmp.pdf")
-
-embed_fonts("tmp.pdf")
--- QnA/hangul.R
+++
@@ -1,9 +0,0 @@
-Sys.setlocale("LC_ALL", "korean")
-# Sys.setlocale("LC_CTYPE", "ko_KR.UTF-8");
-# txt <- readLines(file("file.txt", encoding = "EUC-KR"))
-
-
-# http://r-bong.blogspot.com/2016/03/rstudio_26.html
-
-# In bash shell
-LANG=ko_KR; export LANG   
