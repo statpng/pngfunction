be07b33e35da7298a93c3e561b4a1b2bae9580bc
Kipoong Kim, Ph.D <43024819+statpng@users.noreply.github.com>
2023-04-19 10:58:13 +0900

    Update functions.R

--- mesh3d/functions.R
+++ mesh3d/functions.R
@@ -5,6 +5,113 @@
       paste0(x, y)
 }
 
+png_read.vtk<-function(filename, item = c("points", "triangles", "normals")){
+  item=match.arg(item)
+  
+  if(!file.exists(filename)) stop("Cannot read: ",filename)
+  con=file(filename,open='rb',encoding='ASCII')
+  on.exit(close(con))
+  magic=readLines(con,n=1)
+  if(regexpr("# vtk DataFile Version [2345]",magic,ignore.case =T)<0)
+    stop("Bad header line in file: ",filename)
+  
+  title=readLines(con,1)
+  encoding=readLines(con,1)
+  if(regexpr("ASCII",encoding,ignore.case=TRUE)<0)
+    stop("Can only read ASCII encoded VTK pointsets")
+  
+  datasetLine=toupper(readLines(con,1))
+  if(regexpr("^DATASET",datasetLine)<0)
+    stop("Missing DATASET line")
+  
+  datasetType=sub("DATASET\\s+(\\w+)","\\1",datasetLine)
+  
+  validDatasetTypes<-c("STRUCTURED_POINTS", "STRUCTURED_GRID",
+                       "UNSTRUCTURED_GRID", "POLYDATA", "RECTILINEAR_GRID", "FIELD")
+  
+  if(!datasetType%in%validDatasetTypes)
+    stop(datasetType," is not a valid VTK dataset type")
+  if(datasetType!="POLYDATA")
+    stop("ReadVTKLandmarks can currently only read POLYDATA.",
+         " See http://www.vtk.org/VTK/img/file-formats.pdf for details.")
+  
+  pointsLine=toupper(readLines(con,1))
+  if(regexpr("POINTS",pointsLine)<0)
+    stop("Missing POINTS definition line")
+  ptinfo=unlist(strsplit(pointsLine,"\\s+",perl=TRUE))
+  if(length(ptinfo)!=3)
+    stop("Unable to extract points information from POINTS line",pointsLine)
+  nummarkers=as.integer(ptinfo[2])
+  if(is.na(nummarkers))
+    stop("Unable to extract number of points from POINTS line:",pointsLine)
+  datatype=ptinfo[3]
+  if(!datatype%in%toupper(c("unsigned_char", "char", "unsigned_short", "short", "unsigned_int", "int",
+                            "unsigned_long", "long", "float", "double")))
+    stop("Unrecognised VTK datatype: ",datatype)
+  
+  points=scan(con,what=1.0,n=3*nummarkers,quiet=TRUE)
+  
+  # VTK seems to be hardcoded for 3D
+  if (item == "points"){
+    m=matrix(points,ncol=3,byrow=T)
+    colnames(m)=c("X","Y","Z")
+    attr(m,"file")=filename
+    attr(m,"title")=title
+    attr(m,"vtk_datatype")=datatype
+  }
+  if (item != "points"){
+    toupper(readLines(con,1))
+    triangLine=toupper(readLines(con,1))
+    if(length(triangLine)==0){
+      warning("No data on polygons found")
+      return(NULL)
+    }
+    if(regexpr("POLYGONS",triangLine)<0)
+      stop("Missing POLYGONS definition line")
+    lninfo=unlist(strsplit(triangLine,"\\s+",perl=TRUE))
+    
+    triangDataTypeLine=toupper(readLines(con,1))
+    lnDataTypeinfo=unlist(strsplit(triangDataTypeLine,"\\s+",perl=TRUE))
+    if(length(lninfo)!=3)
+      stop("Unable to extract connection information from POLYGONS line:",triangLine)
+    
+    nummconns=as.integer(lninfo[2])
+    if(is.na(nummconns))
+      stop("Unable to extract number of connections from POLYGONS line:",triangLine)
+    datatype=lnDataTypeinfo[2]
+    triang=scan(con,what=1.0,n=nummconns,quiet=TRUE)
+  }
+  if (item == "triangles"){
+    m=matrix(triang,ncol=3,byrow=T)
+    attr(m,"file")=filename
+    attr(m,"title")=title
+    attr(m,"vtk_datatype")= "int"
+  }
+  # if (item == "normals"){
+  #   normalsLine=toupper(readLines(con,1))
+  #   if(length(normalsLine)==0){
+  #     warning("No data on Normals found")
+  #     return(NULL)
+  #   }
+  #   if(regexpr("NORMALS",normalsLine)<0)
+  #     stop("Missing NORMALS definition line")
+  #   ninfo=unlist(strsplit(normalsLine,"\\s+",perl=TRUE))
+  #   if(length(ninfo)!=3)
+  #     stop("Unable to extract connection information from POLYGONS line",triangLine)
+  #   datatype=ninfo[3]
+  #   if(!datatype%in%toupper(c("unsigned_char", "char", "unsigned_short", "short", "unsigned_int", "int",
+  #                             "unsigned_long", "long", "float", "double")))
+  #     stop("Unrecognised VTK datatype: ",datatype)
+  #   normals=scan(con,what=1.0,n=3*nummarkers,quiet=TRUE)
+  #   m=matrix(normals,ncol=3,byrow=T)
+  #   attr(m,"file")=filename
+  #   attr(m,"title")=title
+  #   attr(m,"vtk_datatype")=datatype
+  # }
+  m
+}
+
+
 png.coord2mesh <- function(nodes, edges, value=NULL, type="triangle"){
   
   data.table::setnames(nodes, c("x", "y", "z"))
